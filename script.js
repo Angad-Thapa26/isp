12d467bd8c356a67d77414eaa67e886551b8479c
angadthapa <119108341+Angad-Thapa26@users.noreply.github.com>
2024-05-06 06:58:23 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const searchButton = document.querySelector('.search-button');
+
+searchButton.addEventListener('click', () => {
+  window.location.href = 'search.html';
+});
+
+const rateButton = document.querySelector('.rate-button');
+
+rateButton.addEventListener('click', () => {
+  window.location.href = 'rate.html';
+});
